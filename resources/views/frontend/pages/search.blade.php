2e013552dce4be690733c615c4e36718eb9d0951
tienminhvy <tienminhvydev@gmail.com>
2023-08-26 12:34:20 +0700

    search template

---
+++ resources/views/frontend/pages/search.blade.php
@@ -0,0 +1,17 @@
+@extends('frontend.layouts.master')
+
+@section('title')
+{{$settings->site_name}} || Tìm kiếm
+@endsection
+
+@section('content')
+    <!--============================
+        SEARCH PAGE START
+    ==============================-->
+    <div id="search">
+        
+    </div>
+    <!--============================
+        SEARCH PAGE END
+    ==============================-->
+@endsection
